b3257f40dec275c505bd74b02dbef3abe2cfeff3
Kayky <kayky7277@gmail.com>
2021-02-09 23:08:35 -0300

    JasoSerializer pode ser usado a partir da versão 3 do dotnet core

---
+++ API_01/API_01/Models/PessoaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_01.Models
+{
+    public class PessoaModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Numero { get; set; }
+    }
+}
